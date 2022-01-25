f01e563cb2279d50c5ddc14aad6d94d131565170
Manish <manishkumar0156@gmail.com>
2022-01-25 16:16:47 +0530

    [Manish] Add: Distance problem solution

---
+++ FunctionalPrograms/FunctionalPrograms/Distance.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class Distance
+    {
+
+        public void calc()
+        {
+            Console.WriteLine("Enter the First Point of the Cooridinate  \n");
+            int x1, y1;
+            string line = Console.ReadLine();
+            string[] numbers = line.Split(' ');
+            x1 = int.Parse(numbers[0]);
+            y1 = int.Parse(numbers[1]);
+
+            Console.WriteLine("Enter the Second Point of the Cooridinate  \n");
+            int x2, y2;
+            line = Console.ReadLine();
+            numbers = line.Split(' ');
+            x2 = int.Parse(numbers[0]);
+            y2 = int.Parse(numbers[1]);
+
+            double f = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("The Euclidan Distance between points is  :" + f);
+
+
+        }
+    }
+}
---
+++ FunctionalPrograms/FunctionalPrograms/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class Program
+    {
+       
+            static void Main(string[] args)
+            {
+                bool flag = true;
+                while (flag)
+                {
+                    Console.WriteLine(" Enter 1 to find the distance between two given points\n " + " Enter 0 to Exit ");
+
+                    flag = true;
+
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    switch (n)
+                    {
+                        case 0:
+                            flag = false;
+                            break;
+
+                        case 1:
+                            Distance distance = new Distance();
+                            distance.calc();
+                            break;
+                    }
+                }
+            }
+       
+    }
+    
+}
+
+    
+
